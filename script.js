a33b6a090425e2d4f7e87f943fbe39f0d6a596cc
Amy Sanders <amysanders@Amys-MacBook-Air.local>
2020-10-21 20:15:18 -0500

    finished studio

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then( function(json) {
+            // let json = [];
+            json.sort(function(a, b) {
+                return a.hoursInSpace - b.hoursInSpace;
+            });
+            const container = document.getElementById("container");
+            for (let i=0; i < json.length; i++) {container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours In Space: ${json[i].hoursInSpace}</li>
+                            <li class="active">Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills.join(', ')}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src=${json[i].picture}>
+                </div>
+                `;
+            };
+            container.innerHTML += `<p><strong>Number of Astronauts: ${json.length}</strong></p>`;
+            let activeColor = document.getElementsByClassName("active");
+            for (let i=0; i < activeColor.length; i++) {
+                if (json[i].active) {
+                activeColor[i].style.color = 'green';
+                };
+            };
+        });
+    });
+});
