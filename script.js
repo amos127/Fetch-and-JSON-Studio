e59a14d2d49565aef10d47e2306ee1ce1161fc24
Amy Sanders <amysanders@Amys-MacBook-Air.local>
2020-10-21 22:48:13 -0500

    fixed sort function

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then( function(json) {
-            // let json = [];
             json.sort(function(a, b) {
-                return a.hoursInSpace - b.hoursInSpace;
+                return b.hoursInSpace - a.hoursInSpace;
             });
             const container = document.getElementById("container");
             for (let i=0; i < json.length; i++) {container.innerHTML += `
